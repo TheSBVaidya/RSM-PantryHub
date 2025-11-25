a32c2ad2905dad134e288a9aecba6de689230104
SanjayV <123719924+TheSBVaidya@users.noreply.github.com>
2025-11-25 11:16:12 +0530

    Delete PantryHubBackend (Monolithic) directory

--- PantryHubBackend/src/main/java/com/pantryhub/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.pantryhub.service;
-
-import com.pantryhub.dto.request.GoogleAuthReqDto;
-import com.pantryhub.dto.response.AuthResDto;
-
-public interface AuthService {
-    AuthResDto loginWithGoogle(GoogleAuthReqDto googleAuthReqDto);
-}
